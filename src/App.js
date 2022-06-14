a8ebee857eea8cdfec2eca8f05c7e96f24ed20cf
austinbhan <austinbhan@outlook.com>
2022-06-14 16:14:12 -0700

    Meal Name Working App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-
 import './App.css';
+import { useState } from 'react';
+import MealNameInput from './MealNameInput';
 
 function App() {
+  const [mealName, setMealName] = useState('');
+
   return (
-    <div className="App">
-      <div id="pictures">
+    <>
+      <h1>
+      Meal Name; {mealName}.
+      </h1>
+      <div className="App">
+        <div id="pictures">
+          <MealNameInput setMealName={setMealName}/>
+        </div>
+        <div id="forms">
 
-      </div>
-      <div id="forms">
+        </div>
+        <div id="display-text">
 
+        </div>
       </div>
-      <div id="display-text">
-
-      </div>
-    </div>
+    </>
   );
 }
 
